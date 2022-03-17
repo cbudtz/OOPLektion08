1ef4dd3dffa2fa8bf5f74fc02bdd97f8a8980c4d
Christian <chrbudtz@gmail.com>
2022-03-17 14:35:21 +0100

    Basic UI

---
+++ src/main/java/gui/FXEvents.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class FXEvents extends Application {
+
+    public static void run(){
+        FXEvents.launch();
+    }
+    @Override
+    public void start(Stage stage) {
+        Button button = new Button("Push Me!");
+        Label label = new Label();
+        FlowPane pane = new FlowPane();
+        pane.getChildren().addAll(label,button);
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
